0e56e95425f3654e7a75702b02fb78cf8b673cd5
Hiroshi Kawana <mutuki@mac.com>
2014-08-10 19:51:53 +0900

    First work version.

--- makeMultiVolumeDisk.sh
+++ makeMultiVolumeDisk.sh
@@ -4,4 +4,12 @@ DISKSIZE=`diskutil list | grep ASD | awk '{printf("%d",$4 + 0.5)}'`
 
 DICKDEVICE=`diskutil list | grep ASD | awk '{print $6}' | cut -c 1-5`
 
-FORMAT=“JournaledHFS+”
+FORMAT=JournaledHFS+
+
+echo $FORMAT
+
+VOLUMESIZE=`expr 100 / $1`%
+
+VERSE=`for ((i=1; i<=$1; i++)); do echo $FORMAT disk$i $VOLUMESIZE ; done`
+
+diskutil partitionDisk $DICKDEVICE $1 GPTFormat $VERSE
