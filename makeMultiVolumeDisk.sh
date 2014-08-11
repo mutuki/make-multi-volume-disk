1ac0272064213e5f4fc297e4f09c2cd75ed85ead
Hiroshi Kawana <mutuki@mac.com>
2014-08-11 18:13:38 +0900

    Update.

--- makeMultiVolumeDisk.sh
+++ makeMultiVolumeDisk.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-DISKSIZE=`diskutil list | grep ASD | awk '{printf("%d",$4 + 0.5)}'`
+VOLUMENAME=`echo $1 | sed -e s/Volumes// | tr -d /`
 
-DICKDEVICE=`diskutil list | grep ASD | awk '{print $6}' | cut -c 1-5`
+echo Target volume name is $VOLUMENAME.
 
-FORMAT=JournaledHFS+
+DISKIDENTIFIER=`diskutil list | grep $VOLUMENAME | awk '{print $6}' | cut -c 1-5`
 
-echo $FORMAT
+echo $VOLUMENAME is $DISKIDENTIFIER.
 
-VOLUMESIZE=`expr 100 / $1`%
+SOURCECOUNT=`ls $2 | grep .dmg | wc -l`
 
-VERSE=`for ((i=1; i<=$1; i++)); do echo $FORMAT disk$i $VOLUMESIZE ; done`
+echo Source “.dmg” files are $SOURCECOUNT.
 
-diskutil partitionDisk $DICKDEVICE $1 GPTFormat $VERSE
+VOLUMESIZE=`expr 100 / $SOURCECOUNT`%
+
+echo Each volume will make $VOLUMESIZE usage on $VOLUMENAME disk space.
+
+VERSE=`for ((i=1; i<=$SOURCECOUNT; i++)); do echo JournaledHFS+ disk$i $VOLUMESIZE ; done`
+
+diskutil partitionDisk $DISKIDENTIFIER $SOURCECOUNT GPTFormat $VERSE
